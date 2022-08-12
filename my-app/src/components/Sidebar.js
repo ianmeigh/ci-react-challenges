71a51f0292b7dc8b729512665727027e79159f79
ianmeigh <ianmeigh@googlemail.com>
2022-08-12 15:28:51 +0100

    📖: Using CSS Modules with React

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./css/Sidebar.module.css"
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className={styles.sidebar}>
       <a href="#">My Photos</a>
       <a href="#">My Illustrations</a>
       <a href="#">My Paintings</a>
